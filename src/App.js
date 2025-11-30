47cbe21b008ac687b41cc2297192498f70d65a45
YeeHong <yhng034@mymail.sim.edu.sg>
2025-11-30 17:05:58 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Features from './components/Feature/Features';
+import About from './components/About/About';
+import Roadmap from './components/Roadmap/Roadmap';
+import Testimonials from './components/Testimonials/Testimonials';
+import Pricing from './components/Pricing/Pricing';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<><Header /><Login /></>} />
+          <Route path="/signup" element={<><Header /><Signup /></>} />
+          <Route path="/" element={
+            <>
+              <Header />
+              <Hero />
+              <Features />
+              <About />
+              <Roadmap />
+              <Testimonials />
+              <Pricing />
+              <Contact />
+              <Footer />
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
